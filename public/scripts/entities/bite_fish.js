9e7ef5fbf87edb2823700bdbbf2154cfcc1e44f7
Mingfeng Li <lmf950122@gmail.com>
2021-06-07 04:15:28 +0000

    Try to add eat feature to bite-fish

--- public/scripts/entities/bite_fish.js
+++ public/scripts/entities/bite_fish.js
@@ -1,6 +1,15 @@
-class BiteFish extends Fish{
+class BiteFish extends GoFish{
   constructor(options) {
     super(options); // Call super to run the code inside `Fish`'s constructor
     this.imageUri = '/images/bite-fish.gif'; // Set the image
+    this.isTasty = false;
+  }
+
+  eat() {
+    let proxPos = this.tank.getProximateDenizen(this.position, this.height * 0.6);
+    let nearFish = proxPos.filter(fish => fish.isTasty);
+    for (let fish of nearFish) {
+      fish.kill();
+    }
   }
 }
